494fbdc2c1081cb08288a16b2038a13b8b40c0df
ephico2real2 <ephico2real@gmail.com>
2024-02-22 02:22:26 -0600

    add ability to delete duplicates

--- flask_dynamic_secrets_app/db/quotes-init.sql
+++ flask_dynamic_secrets_app/db/quotes-init.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE IF NOT EXISTS quotes;
 USE quotes;
 
+-- Create the quotes table with a UNIQUE constraint on the 'quote' column
 CREATE TABLE IF NOT EXISTS quotes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     quote TEXT NOT NULL,
-    author VARCHAR(255) NOT NULL
+    author VARCHAR(255) NOT NULL,
+    UNIQUE(quote(255))
 );
 
-INSERT INTO quotes (quote, author)
+-- Insert a sample quote, ignoring the insertion if the quote already exists
+INSERT IGNORE INTO quotes (quote, author)
 VALUES ('Life is 10% what happens to us and 90% how we react to it.', 'Charles R. Swindoll');
-
